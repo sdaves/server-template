d1ffaa4688df4b7c81fc731fa68b8f51a471f777
Daniel Fagnan <dnfagnan@gmail.com>
2013-04-09 00:34:27 -0600

    starting view API
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

---
+++ index.js
@@ -0,0 +1,70 @@
+/**
+ * Export the `view` function.
+ * @type {Function}
+ */
+
+module.exports = view;
+
+/**
+ * Creates a new view instance.
+ * @param  {String} name View name
+ * @return {View}   View instance.
+ */
+function view(name) {
+  // Views require a name.
+  if (!name) throw new Error("You need to specify a name for the view.");
+
+  // Return a view instance if
+  // it already exists.
+  if (view.views[name])
+    return view.views[name];
+
+  // Create a new view instance.
+  var instance = new View({
+    name: name
+  });
+
+  // Return a new view instance.
+  return view.views[name] = instance;
+}
+
+/**
+ * Registry of all the views.
+ * @type {Object}
+ */
+
+view.views = {};
+
+/**
+ * View constructor.
+ * @param {Object} options View options.
+ */
+
+function View(options) {
+  // View/template name.
+  this.name = options.name;
+  // childView for the current view.
+  this.childView = null;
+}
+
+/**
+ * Add a child view.
+ * @param  {String} child View name
+ * @return {View}   View instance
+ */
+
+View.prototype.child = function(child) {
+  this.childView = child;
+  return this;
+};
+
+/**
+ * Swap the current child view.
+ * @param  {String} name View name
+ * @return {View} View instance
+ */
+
+View.prototype.swap = function(name) {
+  this.childView = view(name);
+  return this;
+};
