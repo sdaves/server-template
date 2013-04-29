f3a7bb1b1646d143654fe10937751801da3cf67f
Lance Pollard <lancejpollard@gmail.com>
2013-04-29 12:02:55 -0700

    ws

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var view = 'undefined' == typeof window ? require('..') : require('tower-view')
   , assert = require('component-assert');
 
 describe('client view', function(){
-  afterEach(function() {
+  afterEach(function(){
     view.clear();
   });
 
@@ -11,27 +11,25 @@ describe('client view', function(){
     assert(view('body') === instance);
   });
 
-  it('should create a new context instance.', function() {
+  it('should create a new context instance.', function(){
     var instance = view.context('global');
     assert(view.context('global') === instance);
   });
 
-  it('should emit `defined` event on view creation.', function(done) {
+  it('should emit `defined` event on view creation.', function(done){
     var newView;
 
     // XXX: Make the test fail if the callback doesn't fire.
-    view.on('defined', function(instance) {
+    view.on('defined', function(instance){
       assert(instance);
       done();
     });
 
     newView = view('newView');
-
   });
 
-  it('should emit `init` event.', function(done) {
-
-    view.on('init', function() {
+  it('should emit `init` event.', function(done){
+    view.on('init', function(){
       assert(true);
       done();
     });
@@ -39,15 +37,14 @@ describe('client view', function(){
     view.init();
   });
 
-  it('should define 1 children view', function() {
-
+  it('should define 1 children view', function(){
     var instance = view('body')
       .child('app');
 
     assert(instance.children[0] === view('app'));
   });
 
-  it('should define more than one child view.', function() {
+  it('should define more than one child view.', function(){
     var instance = view('body')
       .child('app')
       .child('melon');
@@ -56,47 +53,44 @@ describe('client view', function(){
     assert(instance.children[1] === view('melon'));
   });
 
-  it('should return true if view has children.', function() {
+  it('should return true if view has children.', function(){
     var instance = view('body')
       .child('app');
 
     assert(instance.hasChildren() === true);
   });
 
-  it('should return false if view has no children.', function() {
+  it('should return false if view has no children.', function(){
     var instance = view('body');
 
     assert(instance.hasChildren() === false);
   });
 
-  it('should set body element if view name is body', function() {
+  it('should set body element if view name is body', function(){
     view('body');
 
     assert(!! view('body').elem);
   });
 
-  it('should not be rendered.', function() {
+  it('should not be rendered.', function(){
     assert(view('oneT').rendered === false);
   });
 
-  it('should add `render` queue within the runloop', function() {
-    view.run.queues.forEach(function(queue) {
+  it('should add `render` queue within the runloop', function(){
+    view.run.queues.forEach(function(queue){
       if (queue === 'render') assert(queue === 'render');
     });
   });
 
-  it('should trigger `render` queue (runloop)', function(done) {
-
-    view.on('before rendering', function() {
+  it('should trigger `render` queue (runloop)', function(done){
+    view.on('before rendering', function(){
       assert(true);
       done();
     });
 
-    view.run(function() {
-      view.run.batch('sync', {h: 1}, 1233, function() {
+    view.run(function(){
+      view.run.batch('sync', {h: 1}, 1233, function(){
       });
     });
-
   });
-
 });
