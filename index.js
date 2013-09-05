399192ac9bb15401faff9846c9a7bff9908dcef2
Daniel Fagnan <dnfagnan@gmail.com>
2013-09-05 04:38:01 -0600

    added rough stack trace hack
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- index.js
+++ index.js
@@ -4,6 +4,15 @@
 
 var fs = require('fs');
 var path = require('path');
+var stackTrace = require('stack-trace');
+
+/**
+ * Helper
+ */
+
+RegExp.quote = function(str) {
+  return (str + '').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+};
 
 /**
  * Module Exports
@@ -42,29 +51,43 @@ exports.config = function(key, val) {
  */
 
 function view(next) {
-  return function *() {
+  return function * () {
     var self = this;
     this.render = function(file, options) {
-      return function* go() {
+      return function * go() {
         var originFile = file;
+        var trace = stackTrace.parse(self._route.stack);
+        var callingFile = trace[1].getFileName();
+        callingFile = callingFile.split('/').slice(0, -1).join('/')
+
         for (var i = 0, n = exports._config['paths'].length; i < n; i++) {
           var p = exports._config['paths'][i];
 
+          var m = (new RegExp(RegExp.quote(callingFile))).exec(p);
+          if (m) yield render(p);
+        }
+
+        for (var i = 0, n = exports._config['paths'].length; i < n; i++) {
+          var _path = exports._config['paths'][i];
+          yield render(_path);
+        }
+
+        function* render(_path) {
           file = file.replace(/[.]/g, '/');
 
-          var fullPath = path.join(p, file + '.' + exports._config['extension']);
+          var fullPath = path.join(_path, file + '.' + exports._config['extension']);
 
           var found = true;
           try {
             var result = yield read(fullPath);
-          } catch(e) {
+          } catch (e) {
             found = false;
-            continue;
+            return;
           } finally {
             if (!found) {
               self.code = 500;
               self.type = 'text/html';
-              self.body = "<h1>500</h1><h3>We cannot find view: "+originFile+". Tried "+fullPath+"</h3>";
+              self.body = "<h1>500</h1><h3>We cannot find view: " + originFile + ". Tried " + fullPath + "</h3>";
               return;
             } else {
               self.code = 200;
@@ -73,6 +96,7 @@ function view(next) {
             }
           }
         }
+
       }
 
     };
