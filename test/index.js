f96bce1529241869457d87a53005bc4008edd224
Daniel Fagnan <dnfagnan@gmail.com>
2013-09-11 06:23:01 -0600

    tests pass
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- test/index.js
+++ test/index.js
@@ -3,11 +3,13 @@ var view = require('..');
 
 describe('view', function(){
 
-  it('should', function(){
+  it('should should compile simple text directive', function(){
     var r = view('<html><div id="tw" data-text="hello"></div></html>', {
       hello: 123
     });
-    console.log(r);
+    assert(r === '<html><head></head><body><div id="tw" data-text="hello">123</div></body></html>');
   });
+
+
 
 });
